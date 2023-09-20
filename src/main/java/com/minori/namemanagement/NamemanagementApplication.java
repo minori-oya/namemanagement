72ac771f5570b5c0cc0e9df6532ac492bc20a583
minori <minori99525@au.com>
2023-09-20 10:27:23 +0900

    プロジェクトをセットアップ

---
+++ src/main/java/com/minori/namemanagement/NamemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.minori.namemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamemanagementApplication.class, args);
+	}
+
+}
