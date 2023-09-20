01e6565a5d673a56f3d76a3c2ead4361df83d5ca
minori <minori99525@au.com>
2023-09-20 13:16:01 +0900

    Docker周りの設定を追加

--- src/main/java/com/minori/namemanagement/NamemanagementApplication.java
+++ src/main/java/com/minori/namemanagement/NamemanagementApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NamemanagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NamemanagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NamemanagementApplication.class, args);
+    }
 
 }
